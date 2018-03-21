68ca513319d6387ac69e87ca0c1deadd14a2dd85
Mike Monahan <35550407+maiki-mo@users.noreply.github.com>
2018-03-21 18:17:43 -0400

    modal and icon

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let modal = document.getElementById('simpleModal');
 let drwPap = document.getElementById('box1');
 let cmpPap = document.getElementById('box2');
 let brnPap = document.getElementById('box3');
+let body = document.getElementById("body");
 let closeBtn = document.getElementsByClassName('closeBtn')[0];
 
 drwPap.addEventListener('click', openModal);
@@ -13,14 +14,17 @@ window.addEventListener('click', clickOutside);
 
 function openModal(){
     modal.style.display = 'block';
+    body.style.display = 'none';
 }
 
 function closeModal(){
     modal.style.display = 'none';
+    body.style.display = 'block';
 }
 
 function clickOutside(event){
     if(event.target == modal){
       modal.style.display = 'none';
+      body.style.display = 'block';
     }
 }
